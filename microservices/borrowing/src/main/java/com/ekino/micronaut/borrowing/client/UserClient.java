6d6c2316a1958ae3043149f987c8c36c8c6f3387
Yohan Siguret <yohan.siguret@gmail.com>
2018-12-05 10:34:14 +0100

    add retry

--- microservices/borrowing/src/main/java/com/ekino/micronaut/borrowing/client/UserClient.java
+++ microservices/borrowing/src/main/java/com/ekino/micronaut/borrowing/client/UserClient.java
@@ -5,9 +5,11 @@ import com.ekino.micronaut.borrowing.dto.UserDto;
 import java.util.UUID;
 
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.retry.annotation.Retryable;
 import reactor.core.publisher.Mono;
 
 @Client(id = "user", path = "/users")
+@Retryable(delay = "5ms")
 public interface UserClient extends UserOperations {
 
     @Override
